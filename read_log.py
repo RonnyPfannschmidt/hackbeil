cc0219f408b37dc42226009cc82176d316f31c92
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-03-21 23:55:30 +0100

    add a revision reader class, yaml-ish output

--- read_log.py
+++ read_log.py
@@ -1,55 +1,39 @@
 #!/usr/bin/python
 import sys
-import StringIO
-from svn_dump_reader import read_header, read_entry
+from svn_dump_reader import read_header, read_entry, Revision
+
+
+ignore_paths = [
+    'vendor',
+    'codespeak',
+    'xpython',
+    'trunk/www',
+    'pypy/trunk/www/moininstance',
+    'vpath',
+    'rlcompleter2',
+]
+
+
+def path_filter(node):
+    path = node['Node-path']
+    return any(path.startswith(x) for x in ignore_paths)
+
+def change_filter(node):
+    return node['Node-action'] == 'change'
+
+class InterestingRevision(Revision):
+    filters = Revision.filters + [path_filter, change_filter]
 
 
 dump = open(sys.argv[1], 'r')
+for revision in InterestingRevision.iter_file(dump):
+    print 'rev %s:'% revision.id
+    print '  author:', revision.author
+    print '  log:', repr(revision.message.split('\n')[0])
+    print '  files:',
 
+    for node in revision.nodes:
+        print '    -', node['Node-action'], node['Node-path']
+        if 'Node-copyfrom-path' in node:
+            print '        from', node['Node-copyfrom-path'], node["Node-copyfrom-rev"]
 
-print read_header(dump)
-print read_header(dump)
-print read_entry(dump)
-
-nodes = []
-real = 0
-
-
-while True:
-    try:
-        entry = read_entry(dump)
-    except ValueError:
-        break
-    if 'Revision-number' in entry:
-        if not nodes:
-            continue
-        for node in nodes:
-            print '    ', node['Node-action'], node['Node-path']
-            if 'Node-copyfrom-path' in node:
-                print '        from', node['Node-copyfrom-path'], node["Node-copyfrom-rev"]
-        print 'rev', entry['Revision-number'], 'has %s nodes' % len(nodes)
-        real+=1
-        nodes = []
-    else:
-        if not entry: continue
-        if entry == {'data': '', 'props': {}}: continue
-        next = False
-        for path in [
-                'vendor',
-                'codespeak',
-                'xpython',
-                'trunk/www',
-                'pypy/trunk/www/moininstance',
-                'vpath',
-                'rlcompleter2',
-                'rlcompleter2',
-                ]:
-            if entry['Node-path'].startswith(path):
-                next = True
-        if next:
-            continue
-
-        if entry['Node-action'] == 'change': continue
-        nodes.append(entry)
-
-print 'found', real, 'real nodes'
--- svn_dump_reader.py
+++ svn_dump_reader.py
@@ -53,3 +53,61 @@ def read_entry(fd):
     return headers
 
 
+class Revision(object):
+    filters = [
+            lambda x: x=={'data': '', 'props': {}},
+            ]
+    def __init__(self, entry, nodes=()):
+        self.entry = entry
+        self.nodes = [
+            n for n in nodes
+            if not any(
+                f(n)
+                for f in self.filters
+            )
+        ]
+
+    def __repr__(self):
+        return '<Rev %s, nodes=%s>' % (self.id, len(self.nodes))
+
+    @property
+    def id(self):
+        return int(self.entry['Revision-number'])
+
+    @property
+    def message(self):
+        return self.entry['props'].get('svn:log') or '\n'
+
+    @property
+    def author(self):
+        return self.entry['props'].get('svn:author') or '\n'
+
+    @classmethod
+    def from_fd(cls, fd):
+
+        nodes = []
+
+        for entry in iter(lambda: read_entry(fd), None):
+            if 'Revision-number' in entry:
+                break
+            else:
+                nodes.append(entry)
+
+
+        return cls(entry, nodes)
+
+    @classmethod
+    def iter_file(cls, fd, discard_header=True):
+        if discard_header:
+            read_header(fd) # dump version
+            read_header(fd) # dump uuid
+
+        while True:
+            try:
+                rev = cls.from_fd(fd)
+                # only actual commits with file data
+                if rev.nodes:
+                    yield rev
+            except ValueError:
+                break
+
