f525f57cbfab760a2e72457bcc87945246a448f6
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-03-21 22:45:24 +0100

    move the dump reading functions to a own module

--- read_log.py
+++ read_log.py
@@ -1,54 +1,10 @@
 #!/usr/bin/python
 import sys
 import StringIO
+from svn_dump_reader import read_header, read_entry
+
+
 dump = open(sys.argv[1], 'r')
-
-def headerkv(text):
-    key, value = text.split(':', 1)
-    value = value.strip()
-    if value.isdigit():
-        value = int(value)
-    return key, value
-
-def read_header(fd):
-    return dict(map(headerkv, iter(fd.readline, '\n')))
-
-def read_props_inner(fd):
-    while True:
-        line = fd.readline().strip()
-        if line == 'PROPS-END':
-            return
-        kind, len = line.split(' ')
-        assert kind == 'K'
-        key = fd.read(int(len))
-        fd.read(1) # padding newline
-
-        kind, len = fd.readline().strip().split()
-        assert kind == 'V'
-        value = fd.read(int(len))
-        fd.read(1) # padding newline
-        yield key, value
-
-
-
-def read_props(fd, pl):
-    if not pl:
-        return
-    raw = fd.read(pl)
-    return read_props_inner(StringIO.StringIO(raw))
-
-def read_entry(fd):
-    headers = read_header(fd)
-    cl = headers.get('Content-length', 0)
-    pl = headers.get('Prop-content-length', 0)
-    props = dict(read_props(fd, pl) or [])
-    data = fd.read(cl-pl)
-    headers.update({
-        'props': props,
-        'data': data,
-        })
-    return headers
-
 
 
 print read_header(dump)
