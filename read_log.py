1a86be836040ace1efe69776ec168f4ab1c08abe
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-03-22 11:05:54 +0100

    some more changes to the log reader

--- read_log.py
+++ read_log.py
@@ -11,7 +11,13 @@ ignore_paths = [
     'pypy/trunk/www/moininstance',
     'vpath',
     'rlcompleter2',
+    'epoz',
+    'kupu',
+    'z3',
+    'user',
+    'rr',
 ]
+
 
 
 def path_filter(node):
@@ -22,15 +28,15 @@ def change_filter(node):
     return node['Node-action'] == 'change'
 
 class InterestingRevision(Revision):
-    filters = Revision.filters + [path_filter, change_filter]
+    filters = Revision.filters + [path_filter]
 
 
 dump = open(sys.argv[1], 'r')
 for revision in InterestingRevision.iter_file(dump):
     print 'rev %s:'% revision.id
     print '  author:', revision.author
-    print '  log:', repr(revision.message.split('\n')[0])
-    print '  files:',
+    print '  log:', revision.message.split('\n')[0]
+    print '  files:'
 
     for node in revision.nodes:
         print '    -', node['Node-action'], node['Node-path']
