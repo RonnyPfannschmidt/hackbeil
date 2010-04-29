5e4d0f40c26a8e1ca9b5075c91ddebaf2e2e201d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-04-30 00:25:57 +0200

    add utility function for creating window views

---
+++ test_utils.py
@@ -0,0 +1,12 @@
+import py
+from utils import window
+def test_window():
+
+    w = window(range(10), 6)
+    for item in w:
+        print item
+    w = window(range(10), 8)
+    assert w.next() == tuple(range(8))
+    assert w.next() == tuple(range(1,9))
+    assert w.next() == tuple(range(2, 10))
+    py.test.raises(StopIteration, w.next)
