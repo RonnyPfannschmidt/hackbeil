fe4e880d8b97371e4e9f1c0b648e0ebf7e5a16c2
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-25 23:54:12 +0200

    switch the json tree writer to argpase file types

--- hb-dump2jsontree.py
+++ hb-dump2jsontree.py
@@ -3,30 +3,32 @@
 import argparse
 import json
 
-from os.path import join
+from os.path import join, exists
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('dump')
-parser.add_argument('output')
+parser.add_argument(
+    'dump', type=argparse.FileType('r'),
+)
+parser.add_argument(
+    'output', type=argparse.FileType('w'),
+)
 parser.add_argument('file_tree')
 
 from hackbeil.svn_dump_reader import read_header, walk_entries
 
 options = parser.parse_args()
 
-dump = open(options.dump)
-output = open(options.output, 'w')
-
-read_header(dump) # skip uuid
-read_header(dump) # skip extra metadatoa
-
-for entry in walk_entries(dump):
+for entry in walk_entries(options.dump):
+    if 'revno' in entry:
+        print 'rev', entry['revno']
     data = entry.pop('data', None)
     if data is not None:
-        with open(join(options.file_tree, entry['content_sha1']), 'w') as fp:
-            fp.write(data)
-    print >> output, json.dumps(entry)
+        path = join(options.file_tree, entry['content_sha1'])
+        if not exists(path):
+            with open(path ,'w') as fp:
+                fp.write(data)
+    print >> options.output, json.dumps(entry)
 
 
 
