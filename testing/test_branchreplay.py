fc36af65b08562b49a15037dfaad0621b7d6eb68
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-29 15:18:18 +0200

    some more events

--- testing/test_branchreplay.py
+++ testing/test_branchreplay.py
@@ -9,20 +9,46 @@ def test_simple_replay(replay):
     assert not replay.branch_history
     assert not replay.branches
 
-    replay.rev = 0
-
-    replay.action(
+    replay.event(
         action='add',
         kind='dir',
         path='trunk',
     )
     assert 'trunk' in replay.branches
 
-    replay.rev = 1
-    replay.action(
+    previous = replay.revdone()
+
+
+    replay.event(
+        action='add',
+        kind='dir',
+        path='branch/test',
+        copy_from='trunk',
+        copy_rev=previous,
+    )
+
+
+
+    replay.revdone(nextrev=100)
+    replay.event(
         action='delete',
         path='trunk',
     )
 
     assert 'trunk' not in replay.branches
-    assert replay.branch_history[0].endrev == 1
+    assert replay.branch_history[0].endrev == 100
+
+    previous = replay.revdone()
+
+    replay.event(
+        action='delete',
+        path='branch/test',
+    )
+    replay.event(
+        action='add',
+        kind='dir',
+        path='trunk',
+        copy_from='branch/test',
+        copy_rev=previous,
+    )
+    replay.revdone()
