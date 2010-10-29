e71bc4934ff5f31522e18cb5b80f543b6fa7fb2a
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-29 13:50:53 +0200

    move stitching the first revision to the hgutils package

--- scripts/stitcher.py
+++ scripts/stitcher.py
@@ -41,55 +41,15 @@ ui.status('found %s:%s (%s)\n' % (current.rev(),
                                   current.hex(),
                                   current.extra().get('convert_revision')))
 
-ui.status('comparing first change and source parent\n')
-
-stitch_root = stitch_source[0]
-new_files = set(stitch_root)
-old_files = set(current)
-
-common = sorted(old_files&new_files)
-
-added = new_files-old_files
-removed = old_files-new_files
-changed = set(
-    name for name in common
-    if current[name].data() != stitch_root[name].data()
-)
-
-ui.status('added %s removed %s changed %s common %s\n\n' % (
-    len(added),
-    len(removed),
-    len(changed),
-    len(common),
-))
-
-
-
-
-
-
-ui.status('stitching initial revision\n')
 
 tr = target_repo.transaction('commit')
 
-base_extra = stitch_root.extra()
-if options.import_as_branch:
-    base_extra['branch'] = options.import_as_branch
-
-memctx = context.memctx(
-    repo=target_repo,
-    parents=[current.node(), None],
-    text=stitch_root.description(),
-    user=stitch_root.user(),
-    date=stitch_root.date(),
-    files=sorted(added|removed|changed),
-    extra=base_extra,
-    filectxfn = hgutils.copying_fctxfn(stitch_root),
-)
-
 with hgutils.abort_on_error(tr):
-    nextnode = target_repo.commitctx(memctx)
-
+    nextnode = hgutils.replay_a_initial_commit(
+        repo=target_repo,
+        base=source_rev,
+        source=stitch_source,
+    )
 
 for index, commit in enumerate(stitch_source):
     # we already took the first commit
