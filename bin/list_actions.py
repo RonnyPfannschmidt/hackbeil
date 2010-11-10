2338ff9c8239d503f498f27ae3e9ad2e5fcf97d4
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-11-10 23:57:33 +0100

    fix the action replay off by one bugs

--- bin/list_actions.py
+++ bin/list_actions.py
@@ -13,8 +13,8 @@ br = BranchReplay.from_json(data)
 er = EventReplay(br)
 er._add_replay()
 
-for chunk in er.generate_chunklist():
-    print chunk
+#for chunk in er.generate_chunklist():
+#    print chunk
 
-#for action, chunk in er.generate_actions():
-#    print action, chunk
+for action, chunk in er.generate_actions():
+    print action, chunk
