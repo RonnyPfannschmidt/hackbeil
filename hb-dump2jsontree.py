59c6269a6be5372ab9a030292a247a245d315f08
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-11-17 23:02:29 +0100

    support progress bars in hb-dump2jsontree

--- hb-dump2jsontree.py
+++ hb-dump2jsontree.py
@@ -2,7 +2,6 @@
 
 import argparse
 import json
-
 from os.path import join, exists
 
 parser = argparse.ArgumentParser()
@@ -16,19 +15,27 @@ parser.add_argument(
 parser.add_argument('file_tree', default=None, nargs='?')
 
 from hackbeil.svn_dump_reader import read_header, walk_entries
-
+from hackbeil.hgutils import progressui
 options = parser.parse_args()
+
+ui = progressui()
+
+
+total = None
 
 for entry in walk_entries(options.dump):
     if 'revno' in entry:
-        print 'rev', entry['revno']
+        if entry['revno'] == 0:
+            total = int(entry['svn:sync-last-merged-rev']) #HACK
+        ui.progress('rev', pos=entry['revno'], total=total)
     data = entry.pop('data', None)
     if data is not None and options.file_tree is not None:
         path = join(options.file_tree, entry['content_sha1'])
         if not exists(path):
             with open(path ,'w') as fp:
                 fp.write(data)
-    print >> options.output, json.dumps(entry)
+    options.output.write(json.dumps(entry) + '\n')
+    options.output.flush()
 
 
 
