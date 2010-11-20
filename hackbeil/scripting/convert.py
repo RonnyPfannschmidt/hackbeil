60d7040b55f3c6a860248e8346b67f5cd43d533c
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-11-20 11:51:29 +0100

    fastpath for converts of closed branches

--- hackbeil/scripting/convert.py
+++ hackbeil/scripting/convert.py
@@ -1,5 +1,7 @@
 from os import path
 import posixpath
+from mercurial.localrepo import localrepository
+from hackbeil.hgutils import svnrev
 def targetdirname(branch):
     return '{base}@{start}'.format(
         base=branch.path.split('/')[-1],
@@ -23,11 +25,22 @@ def do_convert(converter, branch, repo, basedir, authormap):
 def convert_all(ui, replay, convert_call, repo, basedir, authormap):
     for idx, branch in enumerate(replay.branch_history):
         name = targetdirname(branch)
+
         ui.status('%s %s/%s\n'%(name, idx, len(replay.branch_history))),
         ui.progress('converting repos',
                     pos=idx,
                     total=len(replay.branch_history),
                     item=name,
                    )
+        if branch.end:
+            #XXX: fragility?
+            dest = localrepository(ui, path.join(basedir, name))
+            tip = dest['tip']
+            if branch.changesets:
+                if svnrev(tip) == max(branch.changesets):
+                    continue
+            else:
+                continue
+
         do_convert(convert_call, branch, repo, basedir, authormap)
 
